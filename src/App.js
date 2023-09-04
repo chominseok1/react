ad003950ff7fb448349823e0eb8cb13058dbbc14
chominseok1 <mins2713@naver.com>
2023-09-04 17:19:27 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Header from "./layout/Header";
+import Home from "./layout/Home";
+import FoodList from "./food/FoodList";
+import FoodDetail from "./food/FoodDetail";
+import Location from "./seoul/Location";
+import Shop from "./seoul/Shop";
+import Nature from "./seoul/Nature";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <div className={"container"}>
+        <Routes>
+          <Route exact path={"/"} element={<Home/>}/>
+          <Route  path={"/food/food_list/:cno"} element={<FoodList/>}/>
+          <Route  path={"/food/food_detail/:fno"} element={<FoodDetail/>}/>
+            <Route path={"/seoul/location"} element={<Location/>}/>
+            <Route path={"/seoul/shop"} element={<Shop/>}/>
+            <Route path={"/seoul/nature"} element={<Nature/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
