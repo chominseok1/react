b01432958488a8dc5cb86c17fafbf9309eee224e
chominseok1 <mins2713@naver.com>
2023-09-05 15:00:19 +0900

    리액트 3번라인44

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import FoodDetail from "./food/FoodDetail";
 import Location from "./seoul/Location";
 import Shop from "./seoul/Shop";
 import Nature from "./seoul/Nature";
+import Find from "./food/Find";
+import News from "./news/News";
 
 function App() {
   return (
@@ -15,12 +17,14 @@ function App() {
       <Header/>
       <div className={"container"}>
         <Routes>
-          <Route exact path={"/"} element={<Home/>}/>
-          <Route  path={"/food/food_list/:cno"} element={<FoodList/>}/>
-          <Route  path={"/food/food_detail/:fno"} element={<FoodDetail/>}/>
+            <Route exact path={"/"} element={<Home/>}/>
+            <Route  path={"/food/food_list/:cno"} element={<FoodList/>}/>
+            <Route  path={"/food/food_detail/:fno"} element={<FoodDetail/>}/>
             <Route path={"/seoul/location"} element={<Location/>}/>
             <Route path={"/seoul/shop"} element={<Shop/>}/>
             <Route path={"/seoul/nature"} element={<Nature/>}/>
+            <Route path={"/food/find"} element={<Find/>}/>
+            <Route path={"/news/news_find"} element={<News/>}/>
         </Routes>
       </div>
     </Router>
